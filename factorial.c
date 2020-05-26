88999426c28d3889f1dcb00dc2e4ac0f77872072
Chittajallu Chinmai Ganesh <46193776+chcg823@users.noreply.github.com>
2020-05-26 12:13:44 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int factorial(int n);
+
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    printf("Factorial of %d is %d",n,factorial(n));
+    return 0;
+}
+
+int factorial(int n)
+{
+    if(n!=0)
+    {
+        return n*factorial(n-1);
+    }
+    else
+    {
+        return 1;
+    }
+}
